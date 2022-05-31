b7eb8437e533ac482ee7367c1fdee6c4053e2b61
malwinq <malwina.kubas@gmail.com>
2022-05-31 20:47:23 +0200

    usecases

--- ec0lint/src/pages/UseCasesPage.js
+++ ec0lint/src/pages/UseCasesPage.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { useCases } from "../text";
+import References from '../sections/References';
 
 export default function UseCasesPage() {
   return (
@@ -7,8 +8,15 @@ export default function UseCasesPage() {
         <h3 className="sectionTitle" style={{ textAlign: 'center', color: '#8ECCEA' }}>
           Comming soon
         </h3>
-        <h4>{useCases.title}</h4>
-        {/* <h6 className="sectionText" style={{ marginBottom: '40px' }}>{privacyPolicy.text}</h6> */}
+        <h4 className="sectionTitle" style={{ marginBottom: '30px', marginTop: '80px' }}>{useCases.title}</h4>
+        {useCases.bulletPoints.map((x, i) => {
+          return <span>
+              <ul><h5 className="sectionText">{x.title}</h5></ul>
+              <h6 className="sectionText">{x.text}</h6>
+            </span>
+        })}
+        <h6 className="sectionText" style={{ marginBottom: '40px', marginTop: '40px' }}>{useCases.footer}</h6>
+        <References data={useCases.references} />
     </div>
   );
 }
