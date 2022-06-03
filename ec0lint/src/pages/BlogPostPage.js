7c21a7ef5f8bcd5756dd42f313e9c4024e882e48
malwinq <malwina.kubas@gmail.com>
2022-06-03 18:58:27 +0200

    Add arrow to go back

--- ec0lint/src/pages/BlogPostPage.js
+++ ec0lint/src/pages/BlogPostPage.js
@@ -1,9 +1,13 @@
 import React from 'react';
+import { Link } from "react-router-dom";
 import References from '../sections/References';
 
 export default function BlogPostPage(data) {
   return (
     <div className="secondPage">
+      <h5 className="sectionText">
+        <Link to="/blog"className="navLinkBlue"><i>{"<- Go back to all posts"}</i></Link>
+      </h5>
       <h3 className="sectionTitle">
         {data.data.title}
       </h3>
@@ -12,6 +16,9 @@ export default function BlogPostPage(data) {
           return <h6 className="sectionText" style={{ marginBottom: '20px' }}>{x}</h6>;
         })}
       </div>
+      <h5 className="sectionText">
+        <Link to="/blog"className="navLinkBlue"><i>{"<- Go back to all posts"}</i></Link>
+      </h5>
       {data.data.references && <References data={data.data.references} />}
     </div>
   );
