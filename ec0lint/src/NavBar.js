605eac89ad21f1c19c5e75292c4d88b45e37779c
malwinq <malwina.kubas@gmail.com>
2022-06-03 21:43:21 +0200

    add initial menu

--- ec0lint/src/NavBar.js
+++ ec0lint/src/NavBar.js
@@ -2,6 +2,8 @@ import React from "react";
 import { Link, useLocation } from "react-router-dom";
 
 export default function NavBar() {
+  const [displayFeatures, setDisplayFeatures] = React.useState(true);
+
   const location = useLocation();
   const currentPath = location.pathname;
 
@@ -16,11 +18,14 @@ export default function NavBar() {
       <h6 className="headerElement">
         <Link to="/use-cases" className={currentPath === "/use-cases" ? "navLinkPurple" : "navLinkBlue"}>Use Cases</Link>
       </h6>
-      <span class="headerDropdown">
-        <h6 className="headerElement">
-          <Link to="/features" className={currentPath === "/features" ? "navLinkPurple" : "navLinkBlue"}>Features</Link>
+      <h6 className="headerElement" onMouseEnter={() => setDisplayFeatures(true)} onMouseLeave={() => setDisplayFeatures(false)}>
+        <Link to="/features" className={currentPath === "/features" ? "navLinkPurple" : "navLinkBlue"}>Features</Link>
+      </h6>
+      {displayFeatures && <span>
+        <h6 className="headerElement headerDropdown">
+          <Link to="/features/1" className={currentPath === "/features1" ? "navLinkPurple" : "navLinkBlue"}>Features 1</Link>
         </h6>
-      </span>
+      </span>}
       <h6 className="headerElement">
         <Link to="/get-started" className={currentPath === "/get-started" ? "navLinkPurple" : "navLinkBlue"}>Get Started</Link>
       </h6>
